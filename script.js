d9ebb597c794d61bfac149277a2b2c3c55e09798
mirotadzelilu <davidqutateladze@gmail.com>
2022-02-07 21:27:49 +0400

    full

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigationnn = document.getElementById("navigationBlock");
+let buttonnn = document.getElementById("burgerIcon");
+
+buttonnn.addEventListener("click",function() {
+    navigationnn.classList.toggle("chans");
+});
